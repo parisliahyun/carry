a7496ee19d69b2b94670d70b9691f0be86cf8fd8
Paris Lia Hyun <parisliahyun@gmail.com>
2013-08-03 23:01:22 -0700

    set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Carry::Application.routes.draw do
-  get "pages/home"
+root to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
